3958103e88ab0d9a72e2b69731887338d7a0502c
Manas Mahajan <46110357+7heWorrior@users.noreply.github.com>
2019-10-14 22:00:03 +0530

    Add files via upload

--- python/Question1.py
+++ python/Question1.py
@@ -1,5 +1,12 @@
 # modify this function, and create other functions below as you wish
-def question01(initialLevelOfDebt, interestPercentage, repaymentPercentage):
-    # modify and then return the variable below
-    answer = -5
-    return answer
+def question01(intialLevelOfDebt, interestPercentage, repaymentPercentage):
+	fixedPayment = intialLevelOfDebt * (repaymentPercentage/100)
+	debt = intialLevelOfDebt
+	counter = 0
+	while debt>50.00:
+		debt = debt + debt*(interestPercentage/100)
+		debt = debt - fixedPayment
+		counter += 1
+	answer =  counter * fixedPayment + int(debt) + fixedPayment
+	return answer
+
